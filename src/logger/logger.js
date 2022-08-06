fd45747b28e22aa621e3a029de138be82180cf6e
abcikram <roybikram59@gmail.com>
2022-08-06 20:39:55 +0530

    hi

---
+++ src/logger/logger.js
@@ -0,0 +1,5 @@
+function Bikram(){
+
+  console.log("my name is Bikram Roy");
+}
+module.exports.Bikram=Bikram ;
---
+++ src/validator/formatter.js
@@ -0,0 +1,12 @@
+function formatter (){
+
+    let a= "            Bikram Roy        "
+    let b= "My name is Bikram Roy "   
+    let c= "I am a student"
+ }
+    console.log(a.trim())
+    console.log(b.toLowerCase())
+    console.log(c.toUpperCase())
+
+
+    module.exports.formatter=formatter ;
